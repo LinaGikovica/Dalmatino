ef406b8b69aa849985faf5eeced6a729b91bb1b2
LinaGikovica <LinaGikovica@users.noreply.github.com>
2017-12-01 21:42:43 +0100

    Done with js hopefully

--- js/Dalmatino.js
+++ js/Dalmatino.js
@@ -1,1 +1,51 @@
+var interval;
+var countdownTime;
+var deadline;
 
+function nesto() {
+    var work = document.getElementById("worktime");
+    var workvalue = work.value;
+    var mnts = Number(workvalue);
+
+    var timeInMinutes = mnts;
+    var currentTime = Date.parse(new Date());
+    deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
+    timer();
+}
+
+
+function timer() {
+    var currentTime = Date.parse(new Date());
+    countdownTime = deadline - currentTime;
+    interval = setInterval(function() {
+        var hours = Math.floor((countdownTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((countdownTime % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((countdownTime % (1000 * 60)) / 1000);
+        document.getElementById("countdown").innerHTML = "Work for " + hours + " hours " + minutes + " minutes " + seconds + " seconds more."
+
+        if (countdownTime < 0) {
+            clearInterval(interval);
+            document.getElementById("countdown").innerHTML = "Take a break now!";
+        }
+
+        currentTime = Date.parse(new Date());
+        countdownTime = deadline - currentTime;
+    }, 1000);
+}
+
+
+function pause() {
+    clearInterval(interval);
+
+}
+
+function resume() {
+    let currentTime = Date.parse(new Date());
+    deadline = currentTime + countdownTime;
+    timer();
+
+}
+
+function refresh() {
+    window.location.reload();
+}
